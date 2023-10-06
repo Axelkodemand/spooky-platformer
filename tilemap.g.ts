1ebbac776d9694cc981e210d44182ab3da431aac
Axelkodemand <145432595+Axelkodemand@users.noreply.github.com>
2023-10-06 16:08:38 +0200

    Initial files for MakeCode project

---
+++ tilemap.g.ts
@@ -0,0 +1,91 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`640010000303030303030303030303030303030303030303151515151515151515151515151515151515151515171717171717171717171717171717171717171717171b1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1c2222222222222222222222222222222222221111111104040404040404040404040411111111151515151515151515151515151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1e2222222222222222222222222222222223221111111104040411040404041111110411040411151515151515151515151515151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1e2222222222222422222222222222222121211111110404041104041111041111110411110411151515151515151515151515151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f201f1f202222222222222222222222242222222222221104110404111111111104040411111111110404151515151515151515151515151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f212222222222222222222222222222222222220411110411110404111104040411111111111104151515151515151516151616151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f1f1f201f1f1f1f1f1f1f212222242222242222242224222222222222220404040404040404040411111104041111111104151515151615161515151516151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f1f1f1f1f1f201f1f1f1f212222222222222222222222222222222222220404040404040404040404040404040411111111151515151515151515151516151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f212222222222222222222222222222222222220411111104040404040404050505050505050505151516151515151515151516151515151515151515171717171717171717171717171717171717171717171d1f1f1f1f1f1f1f201f1f1f1f1f1f1f1f1f21222222222222222422222222222222222222111104040404040404040404040405051111111115151515151515151515151615151515151515151517171717171717171a1a1717171717171717171717171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f2122222222222222222222222222222222222204040404040404040405040404040405110404111515151515151515151515161515151515151515151717171717171a17171a1717171717171717171a17171d1f1f1f1f201f1f1f1f1f1f1f1f1f1f1f1f212222222222222222222222222222222222220404040409090504040404040a040411110404041515151515151515151515161515151515161515161717171717171717171a1717171717171717171a17171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f21212222222222222222222222222222222222220404040409090504040404040a040411111104041515151515151515151515161515151516151515161717171a1a171717171a1717171717171a17171a17171d1f201f1f1f1f1f1f1f1f1f1f1f1f1f1f212122222222222222222222222222222222222204040504090905040a040b040a040b11111111111515151515151515151515161515151615151515161717171717171717171a1717171717171717171a17171d1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f1f2121222222222222222222222222222222222222080808080c080108080c0808010808080e0f0f101213120d13130d120d0d120d130d120d12130d0d0d191919191919191919191919191919191919191919212121212121212121212121212121212121212102020202020202020202020202020202020206060606060606060606060606060606060606071414141414141414141414141414141414141414141818181818181818181818181818181818181818181821212121212121212121212121212121212121020202020202020202020202020202020202`, img`
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+....................................................................................................
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.dungeon.hazardLava1,sprites.dungeon.hazardWater,myTiles.tile1,sprites.castle.rock0,sprites.skillmap.islandTile7,sprites.skillmap.islandTile8,sprites.skillmap.islandTile4,sprites.castle.rock2,sprites.castle.saplingOak,sprites.swamp.swampTile3,sprites.castle.tileGrass1,sprites.castle.tileDarkGrass2,sprites.castle.tilePath1,sprites.castle.tilePath2,sprites.castle.tilePath3,myTiles.tile2,sprites.castle.tileDarkGrass1,sprites.castle.tileDarkGrass3,myTiles.tile3,myTiles.tile4,myTiles.tile5,myTiles.tile6,myTiles.tile7,myTiles.tile8,myTiles.tile9,sprites.dungeon.darkGroundNorthWest0,sprites.dungeon.darkGroundNorthEast0,sprites.dungeon.darkGroundWest,sprites.dungeon.darkGroundEast,sprites.dungeon.darkGroundCenter,sprites.dungeon.collectibleRedCrystal,sprites.dungeon.floorLight0,myTiles.tile10,myTiles.tile12,myTiles.tile13], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+            case "myTile9":
+            case "tile11":return tile11;
+            case "myTile":
+            case "tile1":return tile1;
+            case "myTile0":
+            case "tile2":return tile2;
+            case "myTile1":
+            case "tile3":return tile3;
+            case "myTile2":
+            case "tile4":return tile4;
+            case "myTile3":
+            case "tile5":return tile5;
+            case "myTile4":
+            case "tile6":return tile6;
+            case "myTile5":
+            case "tile7":return tile7;
+            case "myTile6":
+            case "tile8":return tile8;
+            case "myTile7":
+            case "tile9":return tile9;
+            case "myTile8":
+            case "tile10":return tile10;
+            case "myTile10":
+            case "tile12":return tile12;
+            case "myTile11":
+            case "tile13":return tile13;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
